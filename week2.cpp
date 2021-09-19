5e16174c4c39ed2530a98ebe975bf5f12df35c17
shelby1705 <90979507+shelby1705@users.noreply.github.com>
2021-09-19 11:13:33 -0400

    Finished Constructors

--- week2.cpp
+++ week2.cpp
@@ -1,8 +1,53 @@
 
 #include<iostream>
 
-int main(int argc, char **argv)
-{
-   std::cout << "Hello World!" << std::endl;
-   return 0;
-}
+using namespace std;
+
+//Name Space
+namespace CST8219 {
+
+    //Class
+    class vehicle {
+
+    //Variables
+    private:
+        int numWheels;
+        int numDoors;
+
+     //Constructors
+    public:
+        vehicle(int w, int d) {
+            numWheels = w;
+            numDoors = d;
+
+            cout << "In constructor with 2 parameters" << endl;
+        }//End of constructor a
+
+    public:
+        vehicle(int w) : vehicle(4, w) {
+
+            cout << "In constructor with 1 parameter, wheels = " << w << endl;
+
+        }//End of constructor b
+    
+
+    public:
+        vehicle() : vehicle(4) {
+            cout << "In constructor with 0 parameters" << endl;
+        
+        }//End of constructor c
+
+       
+
+    };//End of vehicle class
+
+
+}//End of nameSpace
+
+//Main
+int main(int argc, char** argv) {
+
+  
+    cout << "I made a vehicle" << endl;
+    return 0;
+}//End of main
